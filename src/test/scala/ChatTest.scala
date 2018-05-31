163c543e506c60efcd928633d4baa04fedf92fcd
Crandel <cradlemann@gmail.com>
2018-05-31 15:42:50 +0300

    Move common test code and add few new tests

--- src/test/scala/ChatTest.scala
+++ src/test/scala/ChatTest.scala
@@ -4,14 +4,39 @@ import chat.WebService
 
 class ChatTest extends FunSuite with Matchers with ScalatestRouteTest {
 
-  test("ping webserver") {
+  def assertSocketConnection()(assertions: (WSProbe) => Unit) = {
     val webService = new WebService()
     val wsClient = WSProbe()
-
     WS("/ws_api", wsClient.flow) ~> webService.routes ~>
-      check {
-        wsClient.sendMessage("""{"$type":"ping","seq":1}""")
-        wsClient.expectMessage("""{"$type":"pong","seq":1}""")
-      }
+      check(assertions(wsClient))
   }
+
+  test("ping webserver") {
+    assertSocketConnection() { wsClient =>
+      wsClient.sendMessage("""{"$type":"ping","seq":1}""")
+      wsClient.expectMessage("""{"$type":"pong","seq":1}""")
+    }
+  }
+
+  test("subscribe to table list") {
+    assertSocketConnection() { wsClient =>
+      wsClient.sendMessage("""{"$type":"subscribe_tables"}""")
+      wsClient.expectMessage("""{"$type":"table_list","tables":[]}""")
+    }
+  }
+
+  test("login successfull") {
+    assertSocketConnection() { wsClient =>
+      wsClient.sendMessage("""{"$type": "login","username": "admin","password": "admin"}""")
+      wsClient.expectMessage("""{"$type":"login_successful","user_type":"admin"}""")
+    }
+  }
+
+  test("login failed") {
+    assertSocketConnection() { wsClient =>
+      wsClient.sendMessage("""{"$type": "login","username": "test user","password": "some pass"}""")
+      wsClient.expectMessage("""{"$type":"login_failed"}""")
+    }
+  }
+
 }
